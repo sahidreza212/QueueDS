37b9cb4e0a6841134ba969b5486ca761c1eaefc2
Sahid Reza <sahidreza976@gmail.com>
2026-07-18 12:39:28 +0530

    using ArrayDeque

--- src/main/java/org/example/QueueCF.java
+++ src/main/java/org/example/QueueCF.java
@@ -1,12 +1,13 @@
 package org.example;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class QueueCF {
+
     public static void main(String[] args) {
 
-        Queue<Integer> q = new LinkedList<>();
+       // Queue<Integer> q = new LinkedList<>();
+       Queue<Integer> q = new ArrayDeque<>();
 
         q.add(1);
         q.add(2);
